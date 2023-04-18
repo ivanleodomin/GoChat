a169eca79c8f442421f0b422e3f8b895134e0c94
ivanleodomin <ivanleodomin00@gmail.com>
2023-04-18 10:35:34 -0300

    directory structure

---
+++ cmd/api/boostrap/boostrap.go
@@ -0,0 +1,13 @@
+package boostrap
+
+import "app-go/internal/platform/server"
+
+const (
+	host = "localhost"
+	port = 8080
+)
+
+func Run() error {
+	srv := server.New(host, port)
+	return srv.Run()
+}
